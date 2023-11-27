8aa51b65926b8cc732a3208b8a126f6be959093c
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-27 12:26:05 +0100

    config: general unstructions

---
+++ Palidroma/scriptt.js
@@ -0,0 +1,10 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//prompt user word
+
+// function to check is user word is palindroma
+
+// print the result in console
