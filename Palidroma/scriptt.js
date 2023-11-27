14d552d3b4d824e8d514ef5536cfc9c9f47ebfcb
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-27 12:43:31 +0100

    feat. function check if the word is palindroma

--- Palidroma/scriptt.js
+++ Palidroma/scriptt.js
@@ -7,10 +7,21 @@ Creare una funzione per capire se la parola inserita è palindroma
 const userWord = prompt("Inserisci qui la parola da verificare")
 
 // function to check is user word is palindroma
+function checkPalindroma (userWord) {
+
+    if (userWord === userWord.split('').reverse().join('')) {
+        return true
+    } else {
+        return false
+    }
+
+}
 
 // call function
+// const result = checkPalindroma(userWord);
 
 // print the result in console
+// console.log(result);
 
 
 
