a70c8fc84878459fbf2da6159727289ae069ed07
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-27 12:36:24 +0100

    feat: prompt userWord

--- Palidroma/scriptt.js
+++ Palidroma/scriptt.js
@@ -4,7 +4,13 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 //prompt user word
+const userWord = prompt("Inserisci qui la parola da verificare")
 
 // function to check is user word is palindroma
 
+// call function
+
 // print the result in console
+
+
+
